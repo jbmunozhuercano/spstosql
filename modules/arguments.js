29011e4a50f6521f852780eb96af37a2b86099e9
Jorge Muñoz Huércano <jbmunoz@protonmail.com>
2024-04-26 19:34:42 +0200

    Cleanup lines

--- modules/arguments.js
+++ modules/arguments.js
@@ -1,23 +1,23 @@
-import yargs from 'yargs/yargs';
+import yargs from 'yargs/yargs'
 
 const argv = yargs(process.argv.slice(2))
-	.option('filename', {
-		demandOption: true,
-		alias: 'fn',
-		type: 'string',
-		describe: 'Nombre del archivo a tratar'
-	})
-	.option('tablename', {
-		demandOption: true,
-		alias: 'tb',
-		type: 'string',
-		describe: 'Nombre de la tabla'
-	})
-	.option('id',{
-		demandOption: true,
-		alias: 'k',
-		type: 'string',
-		describe: 'Nombre del id para la cláusula WHERE'
-	}).argv;
+  .option('filename', {
+    demandOption: true,
+    alias: 'fn',
+    type: 'string',
+    describe: 'Nombre del archivo a tratar',
+  })
+  .option('tablename', {
+    demandOption: true,
+    alias: 'tb',
+    type: 'string',
+    describe: 'Nombre de la tabla',
+  })
+  .option('id', {
+    demandOption: true,
+    alias: 'k',
+    type: 'string',
+    describe: 'Nombre del id para la cláusula WHERE',
+  }).argv
 
-export {argv};
+export { argv }
