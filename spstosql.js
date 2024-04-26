df6433f4599f0c3d75b1dea21329f59b26dc00b0
Jorge Muñoz Huércano <jbmunoz@protonmail.com>
2024-04-26 19:35:01 +0200

    Added async functions

--- spstosql.js
+++ spstosql.js
@@ -1,50 +1,15 @@
-import fs from 'fs';
-import xlsx from 'xlsx';
-import {parse} from 'csv-parse';
-import {todayDate, createQueryLine} from './modules/utils.js';
-import {argv} from './modules/arguments.js';
+import xlsxToSql from './modules/xlsx.js'
+import csvToSql from './modules/csv.js'
+import { argv } from './modules/arguments.js'
 
-const extension = argv.filename.split('.')[1];
-const table_name = argv.tablename;
-const id = argv.id;
-const id_lang = 1;
-const date = todayDate();
+const extension = argv.filename.split('.')[1]
+const table_name = argv.tablename
+const id_lang = 1
 
 if (extension == 'xlsx') {
-	const writeStream = fs.createWriteStream(`${table_name}_${date}.sql`);
-	const file = xlsx.readFile(argv.filename);
-	const sheets = file.SheetNames;
-
-	for (let i = 0; i < sheets.length; i++) {
-		const temp = xlsx.utils.sheet_to_json(
-			file.Sheets[file.SheetNames[i]],
-			{ 
-				range: 1, 
-				header: ['id','meta_title','meta_description']
-			}
-		);
-
-		temp.forEach((line) => {
-			const csvLine = createQueryLine(table_name, line, id, id_lang);
-			//console.log(csvLine + '\r');
-			writeStream.write(csvLine);
-		});
-	}
+  await xlsxToSql(table_name, argv, id_lang)
 } else if (extension == 'csv') {
-	const writeStream = fs.createWriteStream(`${table_name}_${date}.sql`);
-	const delimeter = ',';
-
-	fs.createReadStream(argv.filename)
-	.pipe(parse({
-			delimiter: delimeter,
-			from_line: 2,
-			columns: ['id','meta_title','meta_description']
-		})).on("data", (line) => {
-			const csvLine = createQueryLine(table_name, line, id, id_lang);
-			//console.log(csvLine + '\r');
-			writeStream.write(csvLine);
-	})
+  await csvToSql(table_name, argv, id_lang)
 } else {
-	console.error(`¡La extensión de archivo ${extension} no es válida!`);
+  console.error(`¡La extensión de archivo ${extension} no es válida!`)
 }
-
